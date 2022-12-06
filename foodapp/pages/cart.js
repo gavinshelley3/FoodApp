f6e6f23bf16820021438b70b186fc5c78a39a648
Gavin Shelley <gavinshelley3@hotmail.com>
2022-12-06 14:32:33 -0700

    Tutorial

---
+++ foodapp/pages/cart.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Cart() {
+  return (
+    <>
+      <h1>Cart</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
