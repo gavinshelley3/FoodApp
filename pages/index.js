7d30a689e4f332f6ab2caef57d327a76d045d963
Gavin Shelley <gavinshelley3@hotmail.com>
2022-12-08 19:17:33 -0700

    gh repo

--- pages/index.js
+++ pages/index.js
@@ -60,14 +60,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://github.com/gavinshelley3/FoodApp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          Github Repo
         </a>
       </footer>
     </div>
