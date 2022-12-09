d78bcf1ffc093c4de1e28717365ddbf0fddb62a0
Gavin Shelley <gavinshelley3@hotmail.com>
2022-12-08 21:44:38 -0700

    dlete key

--- pages/components/SearchResult.js
+++ pages/components/SearchResult.js
@@ -4,12 +4,11 @@ import { useState, useEffect } from "react";
 function SearchResult(props) {
   const result = props.result;
   const description = props.description;
-  const key = props.key;
 
   console.log("description: ", description);
   return (
     <Card>
-      <div className={key}>
+      <div>
         <Button>{description}</Button>
       </div>
     </Card>
